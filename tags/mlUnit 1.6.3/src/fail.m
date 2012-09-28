29bf78363e5c7093e48fa54d6f2dd836ddbe0d36
hetu <hetu@7eede3f3-6b80-4757-af92-521a08f45298>
2012-09-28 10:33:58 +0000

    Tagged 1.6.3 release (released 2012-07-13).

---
+++ tags/mlUnit 1.6.3/src/fail.m
@@ -0,0 +1,70 @@
+function fail(msg, varargin)
+%FAIL Raise an error.
+%  FAIL(MSG, varargin) raises a MATLAB error. MSG is a string containing
+%  the fail message. MSG may contain sprintf arguments, which can be
+%  expanded by subsequent arguments in varargin.
+%
+%  The resulting error message will also contain a stack trace.
+%
+%  Examples
+%     % raises a failure with a simple message
+%     fail('Test failed.');
+%
+%     % raises a failure with a sprintf message, expanded by a variable
+%     fail('Test failed, because 3 was not %d.', number);
+
+%  This Software and all associated files are released unter the 
+%  GNU General Public License (GPL), see LICENSE for details.
+%  
+%  $Author$
+%  $Id: fail.m 175 2012-06-12 09:28:58Z alexander.roehnsch $
+
+% set empty message if left empty, or interpret sprintf arguments
+if (nargin == 0)
+   msg_string = '';
+else
+   % avoid multiple sprintf calls on msg in case msg contains masked
+   % sprintf control sequences that should not be interpreted
+   msg_string = sprintf(['\nAssertionError message: ' msg], varargin{:});
+end
+
+% get calling stack with absolute file names
+stack = dbstack('-completenames');
+% But also extract the file names only. The resulting stack.filename are
+% the names of the files each function call occurred, stack.name are the
+% names of each (sub)function, which we do not know or want to filter by.
+[filepaths filenames] = cellfun(@fileparts, {stack.file}, 'UniformOutput', 0);
+[stack.filename] = filenames{:};
+% Pop all internal delegating assert calls from the stack. Assumes that we
+% are getting called through a chain of files named assert, assert_* or
+% fail. Stop at the first item not one of the list names.
+filtered_files = {'assert', ...
+                  'assert_true', ...
+                  'assert_false', ...
+                  'assert_equals', ...
+                  'assert_not_equals', ...
+                  'assert_empty', ...
+                  'assert_not_empty', ...
+                  'assert_error', ...
+                  'fail'};
+while ~isempty(stack) && any(strcmpi(stack(1).filename, filtered_files))
+    stack(1)=[];
+end
+
+% might also have been called from the MATLAB console
+if isempty(stack)
+   stack(1).file = 'MATLABConsole';
+   stack(1).line = NaN;
+end
+
+% Build up the stack trace from here.
+stacktrace = '';
+for i = 1:length(stack)
+    stacktrace = [stacktrace, ...
+        sprintf('\n  In %s at line %d.', stack(i).file, stack(i).line)];  %#ok<AGROW>
+end;
+
+% throw error
+error(['MLUNIT FAILURE:Traceback (most recent call first):', ...
+   stacktrace, ...
+   msg_string]);
