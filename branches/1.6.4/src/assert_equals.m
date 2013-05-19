83f1a4172b3d311f4cb87632800a74e7da1c71f3
axelberres <axelberres@7eede3f3-6b80-4757-af92-521a08f45298>
2013-05-19 16:44:42 +0000

    - add ass- add assert_equals with eps done

--- branches/1.6.4/src/assert_equals.m
+++ branches/1.6.4/src/assert_equals.m
@@ -1,4 +1,4 @@
-function assert_equals(expected, actual, msg, varargin)
+function assert_equals(expected, actual, msg, eps, varargin)
 %ASSERT_EQUALS Raise an error if two expressions do not evaluate to the same.
 %  ASSERT_EQUALS(EXPECTED, ACTUAL) raises a MATLAB error if EXPECTED
 %  and ACTUAL are not the same.
@@ -22,16 +22,25 @@ function assert_equals(expected, actual, msg, varargin)
 %  $Author$
 %  $Id: assert_equals.m 167 2012-06-06 16:10:56Z alexander.roehnsch $
 
-if nargin < 2
+if nargin < 2,
    fail('assert_equals: Not enough input arguments.');
 end
 
-if nargin < 3 || isempty(msg)
+if nargin < 3 || isempty(msg),
    msg = sprintf('Expected <%s>, but was <%s>.', to_string(expected), to_string(actual));
 end
 
-if ~isequal(actual, expected)
-   fail(msg, varargin{:});
+if nargin < 4,
+    eps = 0.001;
+end;
+
+% test the values
+if isa(expected,'double') && isa(actual,'double')    
+    if abs(expected - actual) > eps,
+        fail(msg, 1);
+    end
+elseif ~isequal(actual, expected),
+    fail(msg, varargin{:});
 end
 
 
