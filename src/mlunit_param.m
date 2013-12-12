573336eb96d0051b74cb1f1f049c454fb2a10167
hetu <hetu@7eede3f3-6b80-4757-af92-521a08f45298>
2013-12-12 10:11:40 +0000

    mlunit_param documentation

--- src/mlunit_param.m
+++ src/mlunit_param.m
@@ -1,4 +1,17 @@
 %MLUNIT_PARAM Set or get an mlunit parameter
+%  MLUNIT_PARAM(P) returns the current value of mlUnit's parameter named P.
+%
+%  MLUNIT_PARAM(P, V) changes mlUnit behaviour across multiple test calls in the
+%  same MATLAB instance. P is the name of the mlUnit parameter, V its value.
+%
+%  VOLD = MLUNIT_PARAM(P, V) does the same, but returns the parameter's value
+%  before the change in VOLD.
+%
+%  List of parameters:
+%     'equal_nans' - Logical true handles NaNs equal to each other in
+%                    assert_equals and assert_not_equals calls. Logical false
+%                    handles NaNs not equal to each other. Defaults to false.
+
 %  This Software and all associated files are released unter the 
 %  GNU General Public License (GPL), see LICENSE for details.
 %  
