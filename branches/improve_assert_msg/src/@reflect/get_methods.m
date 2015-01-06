6c08f0a11d56b2aca55f1fc3ea4091f2d84df19e
hetu <hetu@7eede3f3-6b80-4757-af92-521a08f45298>
2015-01-06 14:09:35 +0000

    Branch for improving assert messages.

---
+++ branches/improve_assert_msg/src/@reflect/get_methods.m
@@ -0,0 +1,20 @@
+function meths = get_methods(self)
+%reflect/get_methods returns the list of methods of the 'reflected' class.
+%
+%  Example
+%  =======
+%         r = reflect('test_case');
+%         get_methods(r);           % Returns a cell array with all methods
+%                                   % of the class test_case
+%
+%  See also REFLECT.
+
+%  This Software and all associated files are released unter the 
+%  GNU General Public License (GPL), see LICENSE for details.
+%  
+%  §Author: Thomas Dohmke <thomas@dohmke.de> §
+%  $Id: get_methods.m 23 2006-05-26 23:32:58Z thomi $
+
+meths = methods(self.class_name);
+% delete constructor method
+meths(strcmp(self.class_name, meths)) = [];
