daded658b3973afa3baccca796a056eb3ce2c445
hetu <hetu@7eede3f3-6b80-4757-af92-521a08f45298>
2015-01-29 14:25:41 +0000

    Suppress unnecessary MATLAB warnings.

--- trunk/test/test_assert/test_privates/test_mat2str_char.m
+++ trunk/test/test_assert/test_privates/test_mat2str_char.m
@@ -2,30 +2,30 @@ function test = test_mat2str_char
 
 test = load_tests_from_mfile(test_loader);
 
-function test_noargs
+function test_noargs %#ok<DEFNU>
     
     assert_error(@() mat2str_char());
 
-function test_empty_0x0
+function test_empty_0x0 %#ok<DEFNU>
 
     assert_equals('''''', mat2str_char(''));
     
-function test_empty_1x0
+function test_empty_1x0 %#ok<DEFNU>
 
     % construct 1x0 char array
     s = 'a';
     s(1) = [];
     assert_equals('''''', mat2str_char(s));
 
-function test_scalar
+function test_scalar %#ok<DEFNU>
 
     assert_equals('''foobar''', mat2str_char('foobar'));
 
-function test_matrix
+function test_matrix %#ok<DEFNU>
 
     assert_equals('[''foo'';''bar'']', mat2str_char(['foo';'bar']));
 
-function test_ndims_too_many
+function test_ndims_too_many %#ok<DEFNU>
 
     s(1,1,1) = 'a';
     s(1,1,2) = 'b';
@@ -33,7 +33,7 @@ function test_ndims_too_many
 
 
 %% boilerplate code for testing functions that are private to assert functions
-function set_up
+function set_up %#ok<DEFNU>
 
     assertdir = fileparts(which('assert_error'));
     testdir = fullfile(assertdir, 'private');
@@ -42,7 +42,7 @@ function set_up
     mlunit_param('usertest_strjoin', pwd);
     cd(testdir);
 
-function tear_down
+function tear_down %#ok<DEFNU>
 
     % reset to previous dir
     cd(mlunit_param('usertest_strjoin'));
