c7ec0ef3b5353f89fd00ef0f9ba0bb0d3b9613db
hetu <hetu@7eede3f3-6b80-4757-af92-521a08f45298>
2015-01-30 17:32:04 +0000

    Delete old branches.

--- branches/improve_assert_msg/sample/failure_samples/test_success_output.m
+++
@@ -1,9 +0,0 @@
-function test = test_success_output
-
-test = load_tests_from_mfile(test_loader);
-
-% This test will succeed and is only for multi test suite output demonstration.
-
-function test_success
-
-    a = 3;
