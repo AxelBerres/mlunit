cd8df920987710e13cb3c7e6ffe184c8ddf41996
hetu <hetu@7eede3f3-6b80-4757-af92-521a08f45298>
2015-02-27 11:49:01 +0000

    Fix: gui lag in mlunit_test

--- branches/1.6.x refactoring/test/@test_mlunit_gui/tear_down.m
+++ branches/1.6.x refactoring/test/@test_mlunit_gui/tear_down.m
@@ -6,3 +6,6 @@ function self = tear_down(self)
 %  $Id: tear_down.m 162 2007-01-04 11:38:53Z thomi $
 
 close(self.runner);
+% Give some time for MATLAB to actually close the GUI
+% If not, the GUI remains open until the very end of execution.
+pause(0.1);
