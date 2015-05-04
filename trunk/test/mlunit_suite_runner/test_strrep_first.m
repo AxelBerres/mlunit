5889568a60f8647ee493084599c37a4afa9270af
hetu <hetu@7eede3f3-6b80-4757-af92-521a08f45298>
2015-05-04 09:23:14 +0000

    Fix: Cropped parts of report file package structure, if being executed on relative dir, and the dir name is part of a nested dir.

---
+++ trunk/test/mlunit_suite_runner/test_strrep_first.m
@@ -0,0 +1,56 @@
+function test = test_strrep_first
+
+test = load_tests_from_mfile(test_loader);
+
+function test_empty %#ok<DEFNU>
+    
+    expected = '';
+    result = strrep_first('', 'foo', 'bar');
+    assert_equals(expected, result);
+
+function test_replacement_on_first_position %#ok<DEFNU>
+
+    expected = 'foobar';
+    result = strrep_first('barbar', 'bar', 'foo');
+    assert_equals(expected, result);
+
+function test_no_replacement_on_second_position %#ok<DEFNU>
+
+    expected = 'bbarbar';
+    result = strrep_first('bbarbar', 'bar', 'foo');
+    assert_equals(expected, result);
+    
+function test_no_replacement_if_not_exact %#ok<DEFNU>
+
+    expected = 'baRbar';
+    result = strrep_first('baRbar', 'bar', 'foo');
+    assert_equals(expected, result);
+
+function test_empty_offender_prefixes %#ok<DEFNU>
+
+    expected = 'foobar';
+    result = strrep_first('bar', '', 'foo');
+    assert_equals(expected, result);
+
+    
+function test_cell_mixed_matches %#ok<DEFNU>
+
+    % base and expected
+    data = {...
+        '',         ''; ...
+        'bar',      'foo'; ...
+        'barbar',   'foobar'; ...
+        ' bar',     ' bar'; ...
+        'baR',      'baR'; ...
+    };
+    
+    result = strrep_first(data(:,1), 'bar', 'foo');
+    assert_equals(data(:,2), result);
+
+
+%% boilerplate code for testing private functions
+function set_up %#ok<DEFNU>
+
+    classdir = fileparts(which('mlunit_suite_runner'));
+    testdir = fullfile(classdir, 'private');
+    cd(testdir);
