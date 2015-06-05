0b617ed6a03bb028e8dbfae4460aa68dba881d48
hetu <hetu@7eede3f3-6b80-4757-af92-521a08f45298>
2015-06-05 21:07:36 +0000

    Test case for mlunit_param persistence.

---
+++ trunk/test/utils/test_mlunit_param_persistence.m
@@ -0,0 +1,19 @@
+% Test mlunit_param persistence.
+% Even faced with calls to clear, javaaddpath or some such, parameters must
+% still be present.
+function test = test_mlunit_param_persistence
+
+test = load_tests_from_mfile(test_loader);
+
+% define unique parameter in order to detect reset when it turns out absent
+mlunit_param('mlunittest_persistence', 42);
+
+
+function set_up
+
+    % reset persistent states
+    clear('functions');
+
+function test_persistence
+
+    assert_equals(42, mlunit_param('mlunittest_persistence'));
