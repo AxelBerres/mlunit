8d6a5f063f9900d6b17b45326b6ce21e126b2b75
hetu <hetu@7eede3f3-6b80-4757-af92-521a08f45298>
2015-06-25 09:45:10 +0000

    Elaborate alignment documentation.

--- mlunit/assertions/private/shortest_alignment.m
+++ mlunit/assertions/private/shortest_alignment.m
@@ -5,13 +5,17 @@
 %  
 %  Favors changes over deletions/insertions where ambiguous.
 %  Does not support transpositions.
+%
+%  Distance calculation implemented as described in [1].
+%  Alignment calculation by going backwards a matrix of optimal paths was put on
+%  top of it, and later turned out to be quite close to [2].
+%  [1] https://en.wikipedia.org/wiki/Levenshtein_distance
+%  [2] https://en.wikipedia.org/wiki/Needleman%E2%80%93Wunsch_algorithm
 
 %  This Software and all associated files are released unter the 
 %  GNU General Public License (GPL), see LICENSE for details.
 %  
 %  $Id$
-
-%  TODO: calculate matrix backwards instead of forwards; saves eventual string reversal
 
 function alignment = shortest_alignment(s1, s2)
 
@@ -24,7 +28,12 @@ function alignment = shortest_alignment(s1, s2)
     length1 = length(s1);
     length2 = length(s2);
     
-    % prepare working matrices
+    % Prepare working matrices.
+    % 'distance' is a (n+1 x m+1) array of doubles, which is going to be filled
+    % iteratively with optimal Levenshtein distances.
+    % 'path' is a (n+1 x m+1) array of cells, each of which contains an index
+    % vector indicating the latest index from whence the optimal Levenshtein
+    % distance was taken, describing a path through the matrix.
     distance = repmat(NaN, length1+1, length2+1);
     path = cell(length1+1, length2+1);
     
@@ -40,7 +49,9 @@ function alignment = shortest_alignment(s1, s2)
         path{1, i2} = [1, i2-1];
     end
     
-    % calculate distances and build path matrix
+    % Calculate distances and build path matrix.
+    % Alternatively, the distance matrix may be calculated backwards instead of
+    % forwards in order to save the eventual string reversal.
     for i1 = 1:length1
         for i2 = 1:length2
             if isequal(s1(i1), s2(i2))
@@ -64,11 +75,12 @@ function alignment = shortest_alignment(s1, s2)
         end
     end
     
-    % work out optimal path
+    % Work backwards along the optimal path described in the path matrix,
+    % aligning a1 and a2 on the way.
     step = [length1+1, length2+1];
     pos = 1;
-    a1 = '';
-    a2 = '';
+    a1 = '';        % aligned, but reversed s1
+    a2 = '';        % aligned, but reversed s2
     blank = '·';   % middle dot, 0xB7 in Windows 1252 and ISO 8859-1 encodings
     while ~isequal([0, 0], (path{step(1), step(2)}))
         nextstep = path{step(1), step(2)};
