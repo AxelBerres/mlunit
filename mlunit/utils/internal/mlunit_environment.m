6e0a131f35b7c9f759597c03b5bba6153c22fb58
ferry.bachmann <Ferry.Bachmann@model-engineers.com>
2019-11-28 12:46:26 +0100

    mlunit_environment closes newly loaded block diagrams.
    Why:
    
    For testing functions that work on Simulink models, it is convenient to
    automatically close all Simulink models opened by the test case after
    the test run.
    
    What:
    
    Extended mlunit_environment so that when restoring the environment
    all block diagrams are closed that have been loaded.

--- mlunit/utils/internal/mlunit_environment.m
+++ mlunit/utils/internal/mlunit_environment.m
@@ -3,8 +3,13 @@
 %  You can pass around the state, buffer it, or manage several different states
 %  at the same time.
 %  
-%  The environment state contains the current directory, the current
-%  MATLAB path configuration, and the current mlunit parameter configuration.
+%  The environment state contains the current directory, the current MATLAB
+%  path configuration, the Simulink block diagrams loaded and the current
+%  mlunit parameter configuration.
+%  
+%  When the environment is restored, the block diagrams are closed that are
+%  loaded but not saved in the state. However, no block diagrams are loaded
+%  that are saved in the state but not currently loaded.
 
 %  This Software and all associated files are released unter the 
 %  GNU General Public License (GPL), see LICENSE for details.
@@ -29,6 +34,7 @@ function state = loc_current_environment()
     state.pwd = cd;
     state.config = mlunit_param();
     state.path = path;
+    state.blockdiagrams_loaded = find_system('SearchDepth', 0);
 
 % Reset the environment to the information stored in the state variable.
 function loc_restore_environment(state)
@@ -36,3 +42,6 @@ function loc_restore_environment(state)
     cd(state.pwd);
     mlunit_param(state.config);
     path(state.path);
+    blockdiagrams_loaded_now = find_system('SearchDepth', 0);
+    new_blockdiagrams_loaded = setdiff(blockdiagrams_loaded_now, state.all_bds_loaded);
+    bdclose(new_blockdiagrams_loaded);
