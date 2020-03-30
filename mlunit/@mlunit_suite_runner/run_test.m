d0cb2e53a43ab1abc8c38889259461f0660ccf6e
Zyndric <zyndric@users.noreply.github.com>
2020-03-30 18:12:09 +0200

    Restore R2007b compatibility.

--- mlunit/@mlunit_suite_runner/run_test.m
+++ mlunit/@mlunit_suite_runner/run_test.m
@@ -150,7 +150,7 @@ function result = construct_disabled_result(test)
     result.name = get_function_name(test);
     result.errors = {};
     result.failure = '';
-    [~, reason] = get_disabled(test);
+    [dummy, reason] = get_disabled(test);
     if isempty(reason)
        reason = 'Test disabled.';
     end
