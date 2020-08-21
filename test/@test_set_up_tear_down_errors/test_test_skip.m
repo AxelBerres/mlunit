e07365bc0225a8096b9866a77956117f70b3e25c
Zyndric <zyndric@users.noreply.github.com>
2020-08-21 15:30:18 +0200

    Fix unit test to reflect that skips show no stack anymore

--- test/@test_set_up_tear_down_errors/test_test_skip.m
+++ test/@test_set_up_tear_down_errors/test_test_skip.m
@@ -3,8 +3,7 @@ function self = test_test_skip(self)
 results = assert_executionstate(9, 0, 0, 1);
 
 expected_msg = sprintf([...
-    'This is a runtime skip request in the test.\n' ...
-    'In generic_test.m at line'
+    'This is a runtime skip request in the test.' ...
     ]);
 expected_len = numel(expected_msg);
 assert_equals(expected_msg, results(9).skipped(1:expected_len));
