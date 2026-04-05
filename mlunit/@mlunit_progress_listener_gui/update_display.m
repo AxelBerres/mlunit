b9ed52cf6af99e34e05b7fa7cf53ce31b98bc3a5
Zyndric <zyndric@users.noreply.github.com>
2026-04-05 15:44:12 +0200

    Skip GUI tests if run from GUI itself

--- mlunit/@mlunit_progress_listener_gui/update_display.m
+++ mlunit/@mlunit_progress_listener_gui/update_display.m
@@ -29,7 +29,6 @@ end
 % TODO: disable Run button
 % TODO: make error output selectable and copyable
 % TODO: maintain a recently used list somehow
-% TODO: anything possible about that GUI test itself?
 % TODO: Make message more prominent and distinguish it from the stack trace
 % TODO: Fix awkward wrap behaviour for running '1'
 % TODO: reenable saving mechanism?
--- test/@test_mlunit_gui/set_up.m
+++ test/@test_mlunit_gui/set_up.m
@@ -4,4 +4,9 @@ function self = set_up(self)
 %  §Author: Thomas Dohmke <thomas@dohmke.de> §
 %  $Id: set_up.m 162 2007-01-04 11:38:53Z thomi $
 
+currentstack = dbstack;
+if any(strcmp({currentstack.name}, 'gui_run_callback'))
+    mlunit_skip('Can''t test the mlUnit GUI while running from the mlUnit GUI.');
+end
+
 self.runner = mlunit_gui;
